41bf9d0011807056278bd2f906ef5ee30fa3f7cd
MrRedRed <tomer.trc@gmail.com>
2018-10-21 17:46:38 +0300

    Started coding level editor

---
+++ Level Editor/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WindowsFormsApp1 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose ();
+            }
+            base.Dispose (disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent () {
+            this.components = new System.ComponentModel.Container();
+            this.grid = new Microsoft.VisualBasic.Compatibility.VB6.CheckBoxArray(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.exportPath = new System.Windows.Forms.FolderBrowserDialog();
+            this.export = new System.Windows.Forms.Button();
+            this.saveDialog = new System.Windows.Forms.SaveFileDialog();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.loadDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(24, 37);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(34, 33);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(577, 227);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(34, 33);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(321, 227);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(34, 33);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(24, 227);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(34, 33);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(463, 124);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(34, 33);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(177, 124);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(34, 33);
+            this.checkBox6.TabIndex = 5;
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(577, 37);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(34, 33);
+            this.checkBox7.TabIndex = 6;
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(321, 37);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(34, 33);
+            this.checkBox8.TabIndex = 7;
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // export
+            // 
+            this.export.Location = new System.Drawing.Point(424, 326);
+            this.export.Name = "export";
+            this.export.Size = new System.Drawing.Size(187, 62);
+            this.export.TabIndex = 12;
+            this.export.Text = "Save";
+            this.export.UseVisualStyleBackColor = true;
+            this.export.Click += new System.EventHandler(this.Export_Click);
+            // 
+            // saveDialog
+            // 
+            this.saveDialog.Filter = "C file (*.c)|*.c";
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(24, 326);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(187, 62);
+            this.loadButton.TabIndex = 13;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.lodeButton_Click);
+            // 
+            // loadDialog
+            // 
+            this.loadDialog.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 406);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.export);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Brick Grid Builder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.Compatibility.VB6.CheckBoxArray grid;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.FolderBrowserDialog exportPath;
+        private System.Windows.Forms.Button export;
+        private System.Windows.Forms.SaveFileDialog saveDialog;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.OpenFileDialog loadDialog;
+    }
+}
+
---
+++ Level Editor/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1 {
+    public partial class Form1 : Form {
+        CheckBox[] brickGrid;
+
+        public Form1 () {
+            InitializeComponent ();
+
+            brickGrid = new CheckBox[] {checkBox1, checkBox2, checkBox3, checkBox4, checkBox5, checkBox6, checkBox7, checkBox8};
+        }
+
+        private void Export_Click (object sender, EventArgs e) {
+            saveDialog.ShowDialog ();
+
+            string[] textOfFile = new string[3];
+            textOfFile[0] = "char " + Path.GetFileNameWithoutExtension(saveDialog.FileName) + "[] = {";
+            textOfFile[1] = "\t";
+            Stream file = saveDialog.OpenFile ();
+            for (int i = 0; i < brickGrid.Length - 1; i++) {
+                if (brickGrid[i].Checked) {
+                    textOfFile[1] += "1";
+                } else {
+                    textOfFile[1] += "0";
+                }
+                textOfFile[1] += ", ";
+            }
+            if (brickGrid[brickGrid.Length-1].Checked) {
+                textOfFile[1] += "1";
+            } else {
+                textOfFile[1] += "0";
+            }
+            textOfFile[2] += "};";
+
+            using (StreamWriter streamWriter = new StreamWriter (file)) {
+                foreach (string line in textOfFile) {
+                    streamWriter.WriteLine (line);
+                }
+            }
+        }
+
+        private void button1_Click (object sender, EventArgs e) {
+            exportPath.ShowDialog ();
+        }
+
+        private void Form1_Load (object sender, EventArgs e) {
+
+        }
+
+        private void lodeButton_Click (object sender, EventArgs e) {
+            loadDialog.ShowDialog ();
+
+            Stream file = loadDialog.OpenFile ();
+            string[] textInFile = new string[3];
+
+            using (StreamReader streamReader = new StreamReader (file)) {
+                for (int i = 0; i < 3; i++) {
+                    textInFile[i] = streamReader.ReadLine ();
+                }
+            }
+
+            char[] array = textInFile[1].ToCharArray ();
+            int j = 0;
+            foreach (char character in array) {
+                if (character == '1') {
+                    brickGrid[j].Checked = true;
+                    j++;
+                } else if (character == '0') {
+                    brickGrid[j].Checked = false;
+                    j++;
+                }
+            }
+        }
+    }
+}
